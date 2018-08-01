69b17e7f457097c3d2aa4ccb9295ade4e360fee4
Akshay Dongare <34915737+akshaydongaregit@users.noreply.github.com>
2018-08-02 00:51:04 +0530

    create CheckboxForm.java

---
+++ CheckBoxForm.java
@@ -0,0 +1,19 @@
+package com.cg.trm.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CheckBoxForm {
+
+	private List<String> list = new ArrayList<String>();
+
+	public List<String> getList() {
+		return list;
+	}
+
+	public void setList(List<String> list) {
+		this.list = list;
+	}
+	
+	
+}
